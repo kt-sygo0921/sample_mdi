bf3d9543f7275eb6039426a611f43a9836a31fba
kato shogo <kato.sho@mazda.co.jp>
2022-09-27 18:27:17 +0900

    add typescript emotion

--- pages/about.js
+++
@@ -1,6 +0,0 @@
-const About = () => (
-  <div>
-    <p>aboutページです</p>
-  </div>
-)
-export default About;
---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const StyledP = styled.p`
+  color: red;
+`
+
+const About = () =>  {
+  return (
+    <div>
+      <StyledP>アバウトです</StyledP>
+    </div>
+  )
+}
+
+export default About;
