d869a9174b6472e9dd700364a12c4f9febb343cf
kato shogo <kato.sho@mazda.co.jp>
2022-09-28 18:08:35 +0900

    add sample style

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
+type prpos = {
+  size: "l" | "s"
+}
+
 const StyledP = styled.p`
-  color: red;
+  color: blue;
+`
+
+const StyledP2 = styled.p<prpos>`
+  color: orange;
+  font-size: ${(props) => props.size === "l" ? "20px" : "5px"};
 `
 
 const About = () =>  {
   return (
     <div>
       <StyledP>アバウトです</StyledP>
+      <StyledP2 size='s'>アバウトです</StyledP2>
+      <StyledP2 size='l'>アバウトです</StyledP2>
     </div>
   )
 }
