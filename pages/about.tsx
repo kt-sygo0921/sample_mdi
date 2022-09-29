ff7a62ed9c25f9e2f400ed73968cd94a3cbce3b7
kt-sygo0921 <s.kato2104@gmail.com>
2022-09-29 18:24:44 +0900

    add rest sample

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from '@emotion/styled'
 
-type prpos = {
-  size: "l" | "s"
+type Size = "l" | "s";
+
+type Coffee = {
+  title: String;
+  description: String;
+  ingredients: String[];
+  image: String;
+  id: Number;
+}
+
+type Props = {
+  size: Size
+  coffee: Coffee
+}
+
+type StyledPprops = {
+  size: Size
 }
 
 const StyledP = styled.p`
   color: blue;
 `
 
-const StyledP2 = styled.p<prpos>`
+const StyledP2 = styled.p<StyledPprops>`
   color: orange;
   font-size: ${(props) => props.size === "l" ? "20px" : "5px"};
 `
 
-const About = () =>  {
+const About: React.FC<Props> = (props) =>  {
   return (
     <div>
       <StyledP>アバウトです</StyledP>
       <StyledP2 size='s'>アバウトです</StyledP2>
       <StyledP2 size='l'>アバウトです</StyledP2>
+      <StyledP2 size={props.size ? props.size : 's'}>{props.coffee.title}</StyledP2>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const baseURL = 'https://api.sampleapis.com/coffee/hot';
+  const coffee: Coffee = await fetch(baseURL)
+  .then(resp => resp.json())
+  return {
+    props: {
+      size: 'l',
+      coffee: coffee[0]
+    }
+  }
 }
 
 export default About;
